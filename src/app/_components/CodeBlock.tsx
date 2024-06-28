440800d41e33b2dcbd3df0d7ed77927338b60939
MaaZiJyun <97087257+MaaZiJyun@users.noreply.github.com>
2024-06-28 11:29:49 +0800

    change styles of codeblock

--- src/app/_components/CodeBlock.tsx
+++ src/app/_components/CodeBlock.tsx
@@ -4,6 +4,30 @@ import {
   ClipboardDocumentIcon,
   ClipboardDocumentCheckIcon,
 } from "@heroicons/react/24/outline";
+import { Prism as SyntaxHighlighter } from "react-syntax-highlighter";
+import { githubGist } from "react-syntax-highlighter/dist/esm/styles/hljs";
+
+// Custom CSS style
+const cssStyles = `
+.custom-syntax-pre span {
+  color: #252a34 !important;
+}
+.custom-syntax-pre span.token.keyword {
+  color: #ff2e63 !important;
+}
+.custom-syntax-pre span.token.comment {
+  color: #D2D2D2 !important;
+}
+.custom-syntax-pre span.token.class-name {
+  color: #08d9d6 !important;
+}
+.custom-syntax-pre span.token.function {
+  color: #08d9d6 !important;
+}
+.custom-syntax-pre span.token.operator {
+  color: #ff2e63 !important;
+}
+`;
 
 interface CodeBlockProps {
   children: ReactNode;
@@ -15,42 +39,50 @@ const isCodeElement = (
   return isValidElement(element) && typeof element.props.className === "string";
 };
 
+const extractTextContent = (children: ReactNode): string => {
+  if (typeof children === "string") {
+    return children;
+  } else if (Array.isArray(children)) {
+    return children
+      .map((child) => (typeof child === "string" ? child : ""))
+      .join("");
+  } else if (isValidElement(children)) {
+    return children.props.children
+      ? extractTextContent(children.props.children)
+      : "";
+  } else {
+    return "";
+  }
+};
+
 const CodeBlock: React.FC<CodeBlockProps> = ({ children }) => {
   const [copied, setCopied] = useState(false);
-  const [className, setClassName] = useState<string | null>(null);
+  const [lang, setLang] = useState<string | null>(null);
+  const codeString = extractTextContent(children);
 
   useEffect(() => {
     // Parse children to find the class name of the code element
     if (isValidElement(children) && children.props.className) {
-      const modifiedClassName = capitalizeFirstLetter(
-        children.props.className.replace("lang-", "")
-      );
-      setClassName(modifiedClassName);
+      const modifiedClassName = children.props.className.replace("lang-", "");
+      setLang(modifiedClassName);
     } else if (Array.isArray(children)) {
       const codeElement = children.find(isCodeElement);
       if (codeElement) {
-        const modifiedClassName = capitalizeFirstLetter(
-          codeElement.props.className.replace("lang-", "")
+        const modifiedClassName = codeElement.props.className.replace(
+          "lang-",
+          ""
         );
-        setClassName(modifiedClassName);
+        setLang(modifiedClassName);
       }
     }
   }, [children]);
 
-  const capitalizeFirstLetter = (str: string): string => {
-    return str.charAt(0).toUpperCase() + str.slice(1);
-  };
-
   const handleCopy = () => {
-    const textToCopy =
-      typeof children === "string"
-        ? children
-        : React.Children.toArray(children).join("");
     navigator.clipboard
-      .writeText(textToCopy)
+      .writeText(codeString)
       .then(() => {
         setCopied(true);
-        // setTimeout(() => setCopied(false), 2000);
+        setTimeout(() => setCopied(false), 2000);
       })
       .catch((err) => {
         console.error("Failed to copy code: ", err);
@@ -58,26 +90,35 @@ const CodeBlock: React.FC<CodeBlockProps> = ({ children }) => {
   };
 
   return (
-    <div className="bg-black rounded-lg relative mt-6">
-      <div className="flex justify-between px-4 py-3 bg-white bg-opacity-40">
-        {className && <span className="text-white">{className}</span>}
-        <button onClick={handleCopy} className="text-white">
+    <div className="bg-white mt-6 rounded-lg">
+      <style>{cssStyles}</style> {/* Add custom CSS here */}
+      <div className="code-block-head flex justify-between px-6 py-2 rounded-tl-lg rounded-tr-lg">
+        <span className="text-black">Source Code</span>
+        <button onClick={handleCopy} className="text-black">
           {copied ? (
-            <div className="flex space-x-2 hover:text-gray-200">
+            <div className="flex space-x-2 hover:opacity-50">
               <ClipboardDocumentCheckIcon className="h-5 w-5" />
-              <span className="text-sm">Copied!</span>
+              <span>Copied!</span>
             </div>
           ) : (
-            <div className="flex space-x-2 hover:text-gray-200">
+            <div className="flex space-x-2 hover:opacity-50">
               <ClipboardDocumentIcon className="h-5 w-5" />
-              <span className="text-sm">Copy</span>
+              <span>Copy</span>
             </div>
           )}
         </button>
       </div>
-      <pre className="text-white text-sm m-0 bg-white p-4 rounded-b-lg transition-all bg-opacity-30 hover:bg-opacity-20">
-        {children}
-      </pre>
+      <SyntaxHighlighter
+        language={lang ?? ""}
+        style={githubGist}
+        className="custom-syntax-pre"
+        showLineNumbers={true}
+        wrapLines={true}
+        lineNumberStyle={{ color: "#ccc" }}
+        codeTagProps={{ className: "font-mono" }}
+      >
+        {codeString}
+      </SyntaxHighlighter>
     </div>
   );
 };
